4de34eafeabdd71eba9a035de219f77a14d8725e
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-19 16:24:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
- famous_cats = ["lil' bub", "grumpy cat", "Maru"]
- famous_cats.include?("Maru")
+ array = ["array", "element"]
+ famous_cats.include?("element2")
 end
 
 def using_sort(array)
